99a91f002dcfa986fc5c70ab99bbf5e6dde1909f
web-dev <nathankim137311@gmail.com>
2021-09-03 19:42:50 -0700

    modified the para-container to fit within the screen of a mobile device

--- apps/js/script.js
+++ apps/js/script.js
@@ -15,3 +15,16 @@ function stickyBar () {
 }
 //getElementsByClassName returns an array and not an element
 //window sizing 
+//variables 
+const searchBar = document.querySelector('.search-bar'); 
+const viewWidth = window.matchMedia('(min-width: 700px)');
+
+//eventlisteners
+//functions
+viewWidth.addEventListener('change', event => {
+  if (viewWidth.matches) {
+    searchBar.style.display = 'block';
+  } else {
+    searchBar.style.display = 'none';
+  }
+})
